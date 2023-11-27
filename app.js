34949e1a80f15079321812a3042873c24e3ed174
EcaCosca <enriquecoscarelli@gmail.com>
2023-11-27 15:40:48 +0100

    Done

---
+++ app.js
@@ -0,0 +1,13 @@
+// https://www.w3schools.com/js/js_htmldom_eventlistener.asp
+// document.getElementById("myBtn").addEventListener("click", displayDate);
+
+
+// const target = document.querySelector("#btn1");
+
+// console.log(target);
+
+// target.addEventListener("click", alarm)
+
+function alarm(msg){
+    alert(msg);
+}
